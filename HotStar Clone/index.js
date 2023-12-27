f4b3b7950a8a70aa82f94235c855b22ce5da7dd4
Debjit Singha <125692244+debjitsingha99@users.noreply.github.com>
2023-12-27 16:53:12 +0530

    Add files via upload
    The HotStar Clone project is a robust streaming platform replication inspired by the popular HotStar service. Designed to deliver a seamless and engaging user experience, it encompasses features like video streaming, user authentication, content categorization, and personalized recommendations. Utilizing cutting-edge technologies, this clone ensures high-quality video playback and adaptive streaming. It incorporates user-friendly interfaces for both web and mobile applications, providing accessibility across devices. The project also integrates secure payment gateways for subscription services, making it a comprehensive solution for on-demand entertainment. With a focus on scalability and performance, the HotStar Clone project aims to deliver a rich and immersive streaming experience for users.

---
+++ HotStar Clone/index.js
@@ -0,0 +1,7 @@
+let movies = [
+    {
+        name: "falcon and the winter soldier",
+        des:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet ab eos deserunt, aspernatur quidem hic, quas sequi tenetur autem optio ipsum quis?Commodi doloremque cum magni Eum quam obcaecati mollitia",
+        image: "image/slider 2.png"
+    },
+]
